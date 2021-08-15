84b87d199011a685085c821fd8b030d237bc857d
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-08-15 02:30:24 -0400

    update js
    realised I could clean up the code a bit by not repeating my self like a dummy

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const calc = (num) => {
     if (
         peopleInput.value == "" ||
         billInput.value == "" ||
+        currentPercent == 0 ||
         regex.test(billInput.value)
     ) {
         return;
@@ -41,32 +42,14 @@ const calc = (num) => {
     totalPerPersonOutput.innerHTML = splitTotal.toFixed(2);
 };
 
-// this code runs whenever an input is changed. is is the same as the calculate function but doesn't use the (num) parameter
+//this loops through the inputs and adds an eventlistener on each one. the event triggers anytime an input number is changed. this way you can change the number and the totals will instantly change
 for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("input", function () {
-        if (
-            currentPercent == 0 ||
-            peopleInput.value == "" ||
-            billInput.value == "" ||
-            regex.test(billInput.value)
-        ) {
-            return;
-        }
-
-        let bill = parseFloat(billInput.value);
-        let numPeople = parseInt(peopleInput.value);
-
-        let tip = bill * currentPercent;
-        let splitTip = tip / numPeople;
-
-        let total = bill + tip;
-        let splitTotal = total / numPeople;
-
-        tipOutput.innerHTML = splitTip.toFixed(2);
-        totalPerPersonOutput.innerHTML = splitTotal.toFixed(2);
+    inputs[i].addEventListener("input", () => {
+        calc(currentPercent);
     });
 }
 
+//event listeners for the different percent buttons
 fifteenPercent.addEventListener("click", () => {
     calc(0.15);
 });
