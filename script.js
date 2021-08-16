dbd71b02fad92fd5322c08f10153b2df2df403fc
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-08-15 21:44:54 -0400

    did some tweaking to border radius and some other things

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const billInput = document.querySelector(".bill-input");
 const peopleInput = document.querySelector(".people-input");
-// const inputs = document.querySelectorAll(".inputs");
+const error = document.querySelector(".title-error__h2");
+
 const inputs = document.querySelectorAll("input");
 const tipButtons = document.querySelectorAll(".grid__buttons");
 
@@ -15,7 +16,7 @@ const fifteenPercent = document.getElementById("fifteen");
 const twentyFivePercent = document.getElementById("twenty-five");
 const fiftyPercent = document.getElementById("fifty");
 
-let regex = /[A-Za-z]/;
+let validateInput = /[A-Za-z]/;
 let currentPercent = 0;
 
 peopleInput.value = null;
@@ -26,22 +27,23 @@ totalPerPersonOutput.innerHTML = "$00.00";
 const calc = (num) => {
     currentPercent = num;
 
-    //dont run code if the input field is blank or if it contains letters
+    //if the input field is blank or if it contains letters, dont run the code
     if (
-        // peopleInput.value == "" ||
         billInput.value == "" ||
         currentPercent == 0 ||
-        regex.test(billInput.value)
+        validateInput.test(billInput.value)
     ) {
         return;
     }
 
+    // if the people input is blank or less than or equal to 0, add the css class to activate the error styles
     if (peopleInput.value == "" || peopleInput.value <= 0) {
-        document.querySelector(".title-error__h2").classList.add("active");
-        document.querySelector(".people-input").classList.add("active");
+        error.classList.add("active");
+        peopleInput.classList.add("active");
         return;
     }
 
+    // if all the inputs are filled out and a percent is selected, add the css class to the reset button that brings the opacity back to 1
     if (
         !peopleInput.value == "" &&
         !currentPercent == 0 &&
@@ -50,8 +52,8 @@ const calc = (num) => {
         reset.classList.add("active");
     }
 
-    document.querySelector(".title-error__h2").classList.remove("active");
-    document.querySelector(".people-input").classList.remove("active");
+    error.classList.remove("active");
+    peopleInput.classList.remove("active");
 
     let bill = parseFloat(billInput.value);
     let numPeople = parseInt(peopleInput.value);
@@ -74,6 +76,7 @@ tipButtons.forEach((button) => {
         this.classList.toggle("selected");
     });
 });
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++
 //this loops through the inputs and adds an eventlistener on each one. the event triggers anytime an input number is changed. this way the user can change the bill ammount and the totals will instantly change (based on the currently set tip %)
 inputs.forEach((input) => {
@@ -81,6 +84,25 @@ inputs.forEach((input) => {
         calc(currentPercent);
     });
 });
+
+// event listener for the reset button that removes the error styles / percent styles,  and resets to default values
+reset.addEventListener("click", () => {
+    peopleInput.value = null;
+    billInput.value = null;
+    currentPercent = 0;
+
+    tipOutput.innerHTML = "$00.00";
+    totalPerPersonOutput.innerHTML = "$00.00";
+
+    for (let i = 0; i < tipButtons.length; i++) {
+        tipButtons[i].classList.remove("selected");
+    }
+
+    reset.classList.remove("active");
+    error.classList.remove("active");
+    peopleInput.classList.remove("active");
+});
+
 //+++++++++++++++++++++++++++++++++++++++++++++++++
 //event listeners for the different percent buttons
 
@@ -102,21 +124,4 @@ twentyFivePercent.addEventListener("click", () => {
 
 fiftyPercent.addEventListener("click", () => {
     calc(0.5);
-});
-
-reset.addEventListener("click", () => {
-    peopleInput.value = null;
-    billInput.value = null;
-    currentPercent = 0;
-
-    tipOutput.innerHTML = "$00.00";
-    totalPerPersonOutput.innerHTML = "$00.00";
-
-    for (let i = 0; i < tipButtons.length; i++) {
-        tipButtons[i].classList.remove("selected");
-    }
-
-    reset.classList.remove("active");
-    document.querySelector(".title-error__h2").classList.remove("active");
-    document.querySelector(".people-input").classList.remove("active");
 });
